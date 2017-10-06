89173e3b2bd8cdad1e247b7ce06b3f8ddc99a44b
unknown <etienne32111@hotmail.fr>
2017-10-06 16:10:22 +0200

    first commit

---
+++ src/main/java/LARGOT/labs/App.java
@@ -0,0 +1,13 @@
+package LARGOT.labs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
